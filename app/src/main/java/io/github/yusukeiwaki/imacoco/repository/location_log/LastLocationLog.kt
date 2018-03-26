5097eeb913a87de2b86fe1bd045a0eb29bdf7f72
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2018-03-26 23:42:00 +0900

    remove unused codes and libs

--- app/src/main/java/io/github/yusukeiwaki/imacoco/repository/location_log/LastLocationLog.kt
+++ app/src/main/java/io/github/yusukeiwaki/imacoco/repository/location_log/LastLocationLog.kt
@@ -1,6 +1,6 @@
 package io.github.yusukeiwaki.imacoco.repository.location_log
 
-internal data class LastLocationLog(
+data class LastLocationLog(
         val latitude: Double,
         val longitude: Double,
         val accuracy: Double,
